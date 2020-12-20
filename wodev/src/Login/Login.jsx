12f0a74f62dfa173b3c1b08376e20bb2e0fe7bdc
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@mped.pl>
2020-12-20 19:41:54 +0100

    Add validation to form + unit test

--- wodev/src/Login/Login.jsx
+++ wodev/src/Login/Login.jsx
@@ -1,21 +1,42 @@
+import styled from "styled-components";
 import React from "react";
 import { useForm } from "react-hook-form";
-import './Login.scss'
+import App from "../App";
+import "./Login.scss";
 export default function Login() {
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = (data) => console.log(data);
 
+    const ErrorMessage = styled.span`
+        color: red;
+        margin-left: 5px;
+        margin-top : -5px;
+        margin-bottom : 15px;
+    `;
+
     return (
-        <form className='loginForm' onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group">
-                <label for="login">Login</label>
-                <input id="login" className="form-control form-control-lg" type="text" placeholder="login" name="Login" ref={register({ required: true })} />
-                <label for="password">Password</label>
-                <input id="password" className="form-control form-control-lg" type="text" placeholder="password" name="Password" ref={register({ required: true, minLength: 8 })} />
-            </div>
-            <div className="form-group">
-                <input className="btn btn-primary" type="submit" value="Log in" />
-            </div>
-        </form>
+        <div className="center">
+            <h2 style={{ marginTop: "10vh" }}>Witaj na platformie</h2>
+
+            <h1>WoDev</h1>
+            <form name="credentials" className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-group">
+                    <input id="login" className="form-control form-control-lg input" type="text" placeholder="Login" name="login" ref={register({ required: true })} />
+                    {errors.login && <ErrorMessage>Login is required</ErrorMessage>}
+                    <input
+                        id="password"
+                        className="form-control form-control-lg input"
+                        type="text"
+                        placeholder="Password"
+                        name="password"
+                        ref={register({ required: true, minLength: 8 })}
+                    />
+                    {errors.password?.type === "required" && <ErrorMessage>Password is required</ErrorMessage>}
+                    {errors.password?.type === "minLength" && <ErrorMessage>Password is to short</ErrorMessage>}
+                </div>
+
+                <input className="btn btn-primary fullWidth" type="submit" value="Log in" />
+            </form>
+        </div>
     );
 }
--- wodev/src/__tests__/Login/Login.test.js
+++ wodev/src/__tests__/Login/Login.test.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import Login from '../../Login/Login';
+import React from "react";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import Login from "../../Login/Login";
 
-test("render form",async () => {    
-    const {getByText,getByPlaceholderText} = render(<Login/>);
-    expect(getByText("Log in")).toBeInTheDocument();
-    expect(getByText("Login")).toBeInTheDocument();
-    expect(getByPlaceholderText("login")).toBeInTheDocument();
-    expect(getByText("Password")).toBeInTheDocument();
-    expect(getByPlaceholderText("password")).toBeInTheDocument();
+describe("Login form test", () => {
+    test("render form", async () => {
+        const { getByText, getByPlaceholderText } = render(<Login />);
+        expect(getByText("Log in")).toBeInTheDocument();
+        expect(getByPlaceholderText("Login")).toBeInTheDocument();
+        expect(getByPlaceholderText("Password")).toBeInTheDocument();
+    });
+
+    test("login validation test", async () => {
+        const { getByText } = render(<Login />);
+        act(() => {
+            fireEvent(
+                getByText("Log in"),
+                new MouseEvent("click", {
+                    bubbles: true,
+                    cancelable: true,
+                })
+            );
+        });
+        const loginReq = await waitFor(() => getByText("Login is required"));
+        const passwordReq = await waitFor(() => getByText("Password is required"));
+        expect(loginReq).toBeInTheDocument();
+        expect(passwordReq).toBeInTheDocument();
+    });
+
+    test("login validation password to short", async () => {
+        const { getByText, getByPlaceholderText } = render(<Login />);
+        fireEvent.change(getByPlaceholderText("Password"), { target: { value: "abcde" } });
+        act(() => {
+            fireEvent(
+                getByText("Log in"),
+                new MouseEvent("click", {
+                    bubbles: true,
+                    cancelable: true,
+                })
+            );
+        });
+
+        const password = await waitFor(() => getByText("Password is to short"));
+        expect(password).toBeInTheDocument();
+    });
 });
