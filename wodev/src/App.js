14586489e2329e1daa19552576449701539f10a4
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@mped.pl>
2021-01-09 08:59:54 +0100

    Dodanie routingu, zabezpiecznenie dashborad

--- wodev/src/App.js
+++ wodev/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import Login from './Login/Login'
 import RestorePassword from "./Login/RestorePassword";
+import Home from './Home/Home.jsx';
+import PrivateRoute from "./_components/PrivateRoute";
 
 function App() {
+
     return (
         <Router>
             <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/restorePassword" component={RestorePassword}/>
-                <Redirect exact to="/login"/>
+                <PrivateRoute path="/dashboard" component={Home} exact/>
+                <Route path="/login" component={Login} exact />
+                <Route path="/restorePassword" component={RestorePassword} exact/>
+                <Redirect to="/dashboard"/>
             </Switch>
         </Router>
     );
--- wodev/src/Login/Login.jsx
+++ wodev/src/Login/Login.jsx
@@ -4,26 +4,36 @@ import "./Login.scss";
 import { Button } from "@material-ui/core";
 import { ErrorMessage } from "../Utils/ErrorMessage";
 import { TextButton } from "../Utils/TextButton";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import Logo from "../Resources/Logo.png";
+import { LoginService } from "../Services/LoginService";
+import { useHistory } from 'react-router-dom';
 
 export default function Login() {
     const { register, handleSubmit, watch, errors } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const history = useHistory();
+    const onSubmit = (data) => {
+        let service = new LoginService();
+        service.Login(data).then((res) => {
+            if (res == 200) {
+                history.push("dashboard") 
+            }
+        });
+    };
 
     return (
         <div className="center">
             <h2 style={{ marginTop: "10vh" }}>Witaj na platformie</h2>
-
+            <img src={Logo} className="logoSize" alt=""></img>
             <h1>WoDev</h1>
             <div style={{ margin: "auto" }}>
                 <form name="credentials" className="loginForm" onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-group">
-                        <input id="login" className="form-control form-control-lg input" type="text" placeholder="Login" name="login" ref={register({ required: true })} />
-                        {errors.login && <ErrorMessage>Login is required</ErrorMessage>}
+                        <input className="form-control form-control-lg input" type="text" placeholder="Login" name="login" ref={register({ required: true })} />
+                        {errors.login?.type === "required" && <ErrorMessage>Login is required</ErrorMessage>}
                         <input
-                            id="password"
                             className="form-control form-control-lg input"
-                            type="text"
+                            type="password"
                             placeholder="Password"
                             name="password"
                             ref={register({ required: true, minLength: 8 })}
@@ -32,7 +42,7 @@ export default function Login() {
                         {errors.password?.type === "minLength" && <ErrorMessage>Password is to short</ErrorMessage>}
                     </div>
                     <div className="horizontalGroup">
-                        <Button variant="contained" color="primary" type="submit">
+                        <Button variant="contained" color="primary" type="submit" onClick={handleSubmit(onSubmit)}>
                             Log in
                         </Button>
                     </div>
